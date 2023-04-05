462b371f6be0fdef404e0d1e0b690e86d55f1a35
Ujjawal <ujjawalgupta23@gmail.com>
2023-04-05 14:38:58 +0530

    Add: Author
    Added author class and tested it with TestAuthor

---
+++ python/Author.py
@@ -0,0 +1,23 @@
+class Author:
+    # __name = "ujjawal"
+    
+    def __init__(self, name, email, gender) -> None:
+        self.__name = name
+        self.__email = email
+        self.__gender = gender
+        
+    def getName(self):
+        return self.__name
+
+    def getEmail(self):
+        return self.__email
+
+    def getGender(self):
+        return self.__gender
+    
+    def setEmail(self, email):
+        self.__email= email
+        
+    def __str__(self):
+        return f'{self.__name} ({self.__gender}) at {self.__email}'
+    
---
+++ python/TestAuthor.py
@@ -0,0 +1,7 @@
+from Author import Author
+
+
+anAuthor = Author("Ujjawal", "ujjawalkrgupta@gmail.com", 'm')
+print(anAuthor)
+anAuthor.setEmail("ujjawalgupta23@gmail.com")
+print(anAuthor)
