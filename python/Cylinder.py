accfc21adb5d5486bd34fc270d035ef65cda14a2
Ujjawal <ujjawalgupta23@gmail.com>
2023-04-06 11:23:29 +0530

    Fix: Fixed Cylinder to make it function, and indented TestCylinder

--- python/Cylinder.py
+++ python/Cylinder.py
@@ -5,13 +5,14 @@ from math import pi
 class Cylinder(Circle):
     def __init__(self, radius=1.0, height=1.0) -> None:
         super().__init__(radius)
+        self.radius = radius
         self.__height = height
         
     def get_height(self):
         return self.__height
     
     def get_volume(self):
-        return self.__height * Circle.get_area()
+        return self.__height * super().get_area()
     
     def get_area(self):
-        return 2 * pi * self.__radius * self.__height + 2 * Circle.get_area()
+        return 2 * pi * self.radius * self.__height + 2 * super().get_area()
--- python/TestCylinder.py
+++ python/TestCylinder.py
@@ -5,4 +5,7 @@ c1 = Cylinder()
 print(f'Cyl radius = {c1.get_radius()}, height = {c1.get_height()}, base area = {c1.get_area()}, volume = {c1.get_volume()}')
 
 c2 = Cylinder(2, 6.2)
-print(f'Cyl radius = {c2.get_radius()}, height = {c2.get_height()}, base area = {c2.get_area()}, volume = {c2.get_volume()}')
+print(f'Cyl radius = {c2.get_radius()}, '
+      f'height = {c2.get_height()}, '
+      f'base area = {c2.get_area()}, '
+      f'volume = {c2.get_volume()}')
